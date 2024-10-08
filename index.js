4a59f0bc7ae8632f3beecb08b40ebb9a55ee0ea8
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-10-08 20:58:11 +0700

    Update

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const route=require("./routers/client/index-router")
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+
+// Router
+route(app)
+
+app.listen(port, () => {
+  console.log(`Example app Ongoing on port ${port}`);
+});
+
+
+
