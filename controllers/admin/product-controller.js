5f5acf5793c430752cd660f4a15ee75b8c4c2cb7
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-10-13 20:13:50 +0700

    Search Products Feature

--- controllers/admin/product-controller.js
+++ controllers/admin/product-controller.js
@@ -23,35 +23,58 @@ module.exports.index= async(req, res) => {
         }
     ];
 
+
+    // Check if status is active and finding where the element is active 
     if(req.query.status){
         const index=filterStatus.findIndex(item=>item.status==req.query.status);
 
         if(index!=-1){
-           
+            // Make the current status active by adding the class
             filterStatus[index].class="active";
         }
-        // console.log(index);
+
     }else {
+
         const index=filterStatus.findIndex(item=>item.status=="");
         filterStatus[index].class="active";
     }
 
-    // console.log(req.query.status);
+ 
+
+    // Filter products by status deleted
     let find={
         deleted: false
     };
 
+
+    // Filter products by status if status exists
     if(req.query.status){
+        
+        // Append status to query
         find.status=req.query.status;
     }
 
+    let keyword="";
+
+    if(req.query.keyword){
+        keyword=req.query.keyword;
+        
+        // Append keyword to query
+        const regex=new RegExp(keyword,"i");// i is no special character is lower or upper case
+        find.title=regex;
+    }
+
+    // Query products
     const products= await Product.find(find);
 
     // console.log(products);
 
+
+    // Render view with products and filter status
     res.render("admin/pages/products/index",{
         pageTitle:"Product page",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -23,3 +23,33 @@ if (buttonStatus.length > 0) {
   });
 }
 // End Button Status
+
+
+// Form Search
+const formSearch = document.querySelector("#form-search");
+
+// Check if form search exists
+if (formSearch){
+  let url = new URL(window.location.href);
+
+  formSearch.addEventListener("submit", (e)=>{
+    
+    // Prevent form submission loading 
+    e.preventDefault();
+    const keyword=e.target.elements.keyword.value
+    // console.log(e.target.elements.keyword.value);
+
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    }else{
+      
+      // Remove status query parameter if it exists
+      url.searchParams.delete("keyword");
+    }
+
+    window.location.href = url.href;
+
+  });
+}
+
+// End Form Search
