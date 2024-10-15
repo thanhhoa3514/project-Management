005b5a7db71652b1bf3bb1d645c61019f90a06f2
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-10-15 20:42:55 +0700

    Hiển thị danh sách các sản phẩm bị xoá mềm

--- controllers/admin/restock-controller.js
+++ controllers/admin/restock-controller.js
@@ -1,5 +1,27 @@
-module.exports.restock=(req, res) => {
+const Product = require("../../models/product-model");
+const paginationHelpers = require("../../helpers/pagination");
+
+module.exports.restock=async(req, res) => {
+    let find={
+        deleted: false
+    }
+
+    const countTotalItems=Product.countDocuments(find);
+    let objectPagination = paginationHelpers(
+        {
+            currentPage: req.query.page || 1,
+            limitPage: 4,
+        },
+        req.query,
+        countTotalItems 
+    );
+
+    const products= await Product.find(find).limit(objectPagination.limitPage).skip(objectPagination.skip);
+
+    // console.log(products);
     res.render("admin/pages/restock/index",{
         pageTitle:"Restock page",
+        products: products,
+        pagination: objectPagination,
     });
 };
