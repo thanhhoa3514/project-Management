6fca6bdea658d921b6c92d9e1da34d6ded8adf32
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-10-24 21:57:37 +0700

    tính năng thay đổi trạng thái danh mục nhưng sai chưa làm được

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,5 +1,6 @@
 
 const ProductCategory = require("../../models/product-category.model");
+const filterStatusCategoryHelpers = require("../../helpers/filterStatus");
 const systemConfig=require("../../config/system");
 
 
@@ -7,20 +8,44 @@ const systemConfig=require("../../config/system");
 // [GET] admin/products-category
 module.exports.index = async (req, res) => {
 
+  const filterStatus = filterStatusCategoryHelpers(req.query);
+
     // Filter products by status deleted
     let find = {
       deleted: false,
     };
 
+    // Filter products by status if status exists
+  if (req.query.status) {
+    // Append status to query
+    find.status = req.query.status;
+  }
 
       // Query products
     const records = await ProductCategory.find(find);
 
     res.render("admin/pages/products-category/index", {
       pageTitle: "Category product page",
+      filterStatus: filterStatus,
       records:records
     });
 };
+
+// [PATCH] /admin/products/change-status-category/:inactive/:123
+// module.exports.changeStatusCategory = async (req, res) => {
+
+
+//   const status=req.params.status;
+
+//   const idCategory=req.params.id;
+
+
+//   await ProductCategory.updateOne({_id:idCategory},{status:status});
+
+
+//   res.redirect(req.get("Referrer") || "/");
+// };
+
 // [GET] admin/products-category/create
 module.exports.create = async (req, res) => {
     res.render("admin/pages/products-category/create", {
--- routers/admin/products-category.route.js
+++ routers/admin/products-category.route.js
@@ -8,6 +8,8 @@ const validate = require("../../validates/admin/product-category.validate");
 const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
+// router.patch("/change-status-category/:status/:id", controller.changeStatusCategory);
+
 router.get("/create", controller.create);
 
 router.post(
