c91681446ab530be8d56d6d7ceb622f1d876038c
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-10-27 14:58:18 +0700

    update up ảnh của tiny mce

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -2,6 +2,28 @@
 
 tinymce.init({
   selector: "textarea.textarea-mce",
+  file_picker_callback: (callback, value, meta) => {
+    var input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept",'image/*');
+
+    input.onchange = function() {
+      var file = this.files[0];
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var id="blobid"+(new Date()).getTime();
+        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+        var base64=reader.result.split(",")[1];
+        var blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        callback(blobInfo.blobUri(),{title:file.name});
+      };
+      reader.readAsDataURL(file);
+    };
+    input.click();
+  },
+  file_picker_types: 'file image media',
   plugins: [
     'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
     'searchreplace', 'wordcount', 'visualblocks', 'code', 'fullscreen', 'insertdatetime', 'media',
@@ -14,5 +36,7 @@ tinymce.init({
     favs: { title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons' }
   },
   menubar: 'favs file edit view insert format tools table help',
-  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+
+
 });
