adddb32414e81f0b625506125855217b1dcfbccf
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-11-18 22:46:02 +0700

    Improve interface

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
-console.log("Starting");
+const socialIcons = document.querySelectorAll('.social-icon');  
+    if (socialIcons.length>0){
+    socialIcons.forEach(icon => {  
+        icon.addEventListener('mouseenter', () =>{  
+            this.style.transform = 'scale(1.2)';  
+        });  
+        
+        icon.addEventListener('mouseleave', ()=> {  
+            this.style.transform = 'scale(1)';  
+        });  
+    });  
+}
+
