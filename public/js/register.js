e6cacfb20f33572886aff53b072e88f60ae658c2
thanhhhoa3514 <ngothanhhoa3514@gmail.com>
2024-12-18 16:53:36 +0700

    Make layout register and Login

--- public/js/register.js
+++ public/js/register.js
@@ -125,3 +125,14 @@ document.addEventListener("DOMContentLoaded", () => {
     setupAlert(alertElement.firstElementChild);
   };
 });
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
